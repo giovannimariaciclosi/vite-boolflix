5d37d01a663b70da41c26cdd72949375ef73820b
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-30 14:13:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script >
+import AppHeader from './components/AppHeader.vue';
+
+import { store } from "./store.js";
+import axios from "axios";
+
+
+export default {
+  data() {
+    return {
+
+      store,
+
+    }
+  },
+
+  components: {
+    AppHeader,
+  },
+
+  created() {
+
+  },
+
+  methods: {
+
+  },
+
+}
+
+
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+  name: "AppHeader",
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div>Boolflix</div>
+</template>
+
+<style lang="scss" scoped></style>
